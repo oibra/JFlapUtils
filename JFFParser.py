b6787782395f18b9c470f132c099569a3cfe58f8
oibra <oibrahim.2727@gmail.com>
2025-01-30 14:17:04 -0600

    add CFG parsing to jff parser

--- JFFParser.py
+++ JFFParser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from RegularUtils import DFA, NFA, REGEX
+from CFUtils import CFG
 
 class JFFParser():
   def __init__(self, filename):
@@ -13,7 +14,7 @@ class JFFParser():
     if type != "fa":
       raise AttributeError('input file is not for a finite autoamta!')
 
-    alphabet = parse_alphabet(self.parse_tree, "FA")
+    alphabet = parse_alphabet(self.parse_tree, type)
     states = parse_states(self.parse_tree)
     final_states = parse_final(self.parse_tree)
     start = parse_start(self.parse_tree)
@@ -27,27 +28,75 @@ class JFFParser():
     if type != "fa":
       raise AttributeError('input file is not for a finite autoamta!')
 
-    alphabet = parse_alphabet(self.parse_tree, "FA")
+    alphabet = parse_alphabet(self.parse_tree, type)
     states = parse_states(self.parse_tree)
     final_states = parse_final(self.parse_tree)
     start = parse_start(self.parse_tree)
     if start == None:
       raise AttributeError('no start state')
     transitions, epsilon_transitions = parse_transitions(self.parse_tree, states, alphabet)
-    
     return NFA(Q=states, alpha=alphabet, delta=transitions, e_delta=epsilon_transitions, q0=start, F=final_states)
 
   def generate_regex(self):
-    type = bs_data.find('type').string
+    type = self.parse_tree.find('type').string
     if type != "re":
       raise AttributeError('input file is not for a regular expression!')
     
-    string = bs_data.find('expression').string
+    string = self.parse_tree.find('expression').string
     return REGEX(string)
     
+  def generate_grammar(self):
+    type = self.parse_tree.find('type').string
+    if type != 'grammar':
+      raise TypeError('input file is not for a grammar!')
+    productions = self.parse_tree.find_all('production')
+    variables = parse_variables(productions)
+    terminals = parse_terminals(productions, variables)
+    start = productions[0].find('right').string
+    rules = parse_rules(productions, variables)
+
+    return CFG(start, terminals, variables, rules)
+
+
+def parse_variables(productions):
+  variables = set()
+  for r in productions:
+    variables.add(r.find('left').string)
+  return variables
+
+def parse_terminals(productions, variables):
+  terminals = set()
+  for r in productions:
+    rule = r.find('right').string
+    if rule != None:
+      for v in variables:
+        rule = rule.replace(v, '')
+      terminals.update(list(rule))
+  return terminals
+
+def parse_rules(productions, variables):
+  rules = {v: [] for v in variables}
+  for r in productions:
+    v = r.find('right').string
+    rule = r.find('left').string
+    if rule == None:
+      rules[v].push([])
+    else:
+      rulelist = []
+      while rule != '':
+        start = [n for n in variables if rule.startswith(n)]
+        if len(start) == 0:
+          rulelist.append(rule[0])
+          rule = rule[1:]
+        else:
+          var = [a for a in start if len(a) == max([len(n) for n in start])][0]
+          rulelist.append(var)
+          rule = rule[len(var):]
+      rules[v].push(rulelist)
+
 
 def parse_alphabet(parse_tree, type):
-  if type == "FA":
+  if type == "fa":
     alphabet = set()
     delta = parse_tree.find_all('transition')
     for e in delta:
